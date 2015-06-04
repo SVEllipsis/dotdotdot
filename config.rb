01eabcf7f2f63da4763e4552619fa9099a8ee4f6
Kevin Prince <nextgengames@gmail.com>
2015-06-04 13:37:08 +0100

    Small optimisations

--- config.rb
+++ config.rb
@@ -4,7 +4,6 @@
 compass_config do |config|
   config.output_style = :compact
   config.add_import_path "stylesheets/vendor"
-
 end
 
 
@@ -37,8 +36,15 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
+  compass_config do |config|
+    config.output_style = :compressed
+  end
+  
+  activate :minify_javascript
+
   # Ensure links work even if we're not at the base url
   activate :relative_assets
 
   set :relative_links, true
+
 end
