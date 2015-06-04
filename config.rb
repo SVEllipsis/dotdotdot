1859c9f57f4a94d687e0196956c727ec3d6e8b9d
Kevin Prince <nextgengames@gmail.com>
2015-06-04 12:28:26 +0100

    Fix relative paths and calendar and tag pages

--- config.rb
+++ config.rb
@@ -7,24 +7,32 @@ compass_config do |config|
 
 end
 
-activate :directory_indexes
+
 
 activate :blog do |blog|
   # blog.prefix = "blog"
   blog.permalink = "{year}/{month}/{title}.html"
   blog.sources = "blog/:year-:month-:day-:title.html"
   blog.layout = "blog"
+
+  blog.calendar_template = "calendar.html"
+  blog.tag_template = "tag.html"
 end
+
+activate :directory_indexes
+
 
 activate :deploy do |deploy|
   deploy.build_before = true
   deploy.method = :git
 end
 
+# Ensure links work even if we're not at the base url
+set :relative_links, true
 
 set :css_dir, 'stylesheets'
 
-set :js_dir, 'javascripts'
+set :js_dir, 'js'
 
 set :images_dir, 'images'
 
